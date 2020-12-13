0a616b7d6ff9783b792f2d09c22cfa30f78bba69
s0mnaths <somnaths027@gmail.com>
2020-12-13 13:11:16 +0530

    Update comments

--- task-09/whatsapp_spammer.js
+++ task-09/whatsapp_spammer.js
@@ -4,22 +4,18 @@ var message = "Hi";
 var counter = 20;
 var i=0;
 var interval = 1000; //time in miliseconds
+
 var timer = setInterval(
     function(){
-  
-//for (i = 0; i < counter; i++)     //we can use for loop instead but that will have no delay between messages
-    var event = document.createEvent("Events"); 
-    textBox.innerHTML = message; 
-    event.initEvent("input", true, true, window, 1); 
-    textBox.dispatchEvent(event); 
-    document.querySelector('#main > footer > div._3SvgF._1mHgA.copyable-area > div:nth-child(3) > button > span').click();
+        var event = document.createEvent("Events"); 
+        textBox.innerHTML = message; 
+        event.initEvent("input", true, true, window, 1);  //make send button visible
+        textBox.dispatchEvent(event); 
+        document.querySelector('#main > footer > div._3SvgF._1mHgA.copyable-area > div:nth-child(3) > button > span').click();
     
-//comment out the following if using using for loop
-    i++;
-    if(counter==i)
-    {
-        clearInterval(timer);
-    }
-} , interval)
-
-//}    
+        i++;
+        if(counter==i)
+        {
+            clearInterval(timer);
+        }
+    } , interval)
